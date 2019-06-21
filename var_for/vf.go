e07d8ab1515421b718f382a0f75e7f958c5a5b8e
hitoyozake <mikoto_1122@yahoo.co.jp>
2019-06-21 21:16:13 +0900

    added

---
+++ var_for/vf.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+
+    //var a, b, c int
+    //var x = 1
+    var y = 14
+    //var(
+      //  y, z int
+      //  name string
+    //)
+
+    fmt.Printf("%d\n", y )
+
+}
