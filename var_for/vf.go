8abc207ed77eab673931b6b902a91fc58f4e3165
hitoyozake <mikoto_1122@yahoo.co.jp>
2019-07-10 07:11:52 +0900

    commit

--- var_for/vf.go
+++ var_for/vf.go
@@ -14,4 +14,18 @@ func main(){
 
     fmt.Printf("%d\n", y )
 
+    for i:= 0; i < 5; i++ {
+        fmt.Printf("%d\n", i )
+    }
+    for i:= 4; i > 0; i-- {
+        fmt.Printf("%d\n", i )
+    }
+
+    //初期化する場合は varをつけない
+    sarray := [3] string{"apple", "orange", "peach"}
+
+    for index, s := range sarray {
+        fmt.Printf("%d %s", index, s)
+    }
+
 }
